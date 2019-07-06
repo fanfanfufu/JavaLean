7508eaaf81accfe95b6ae593232f5bafb4931fdd
fanfanfufu <1127137365@qq.com>
2019-07-06 21:38:42 +0800

    0706提交

---
+++ src/main/java/com/fuqi/somequestion/question001.java
@@ -0,0 +1,21 @@
+package com.fuqi.somequestion;
+
+/**
+ * @Description: 两个对象的 hashCode() 相同，则 equals() 也一定为 true，对吗？
+ * @Author: 傅琦
+ * @DateTime: 2019/7/6 16:40
+ * @Version: V1.0
+ */
+public class question001 {
+    /**
+     * 两个对象的 hashCode() 相同
+     * 但是equels方法的返回值却不同
+     * @param args
+     */
+    public static void main(String[] args) {
+        String str1 = "通话";
+        String str2 = "重地";
+        System. out. println(String. format("str1：%d | str2：%d",  str1. hashCode(),str2. hashCode()));
+        System. out. println(str1. equals(str2));
+    }
+}
